af58399610bd1620fa184a544f02eaebed2b22f1
Annabell Sophie Jaeger <a.jaeger@campus.tu-berlin.de>
2024-09-07 00:29:41 +0200

    step 8 to roadpricing_finales Pricing

---
+++ run-script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash --login
+#SBATCH --job-name=test
+#SBATCH --output=test.log
+#SBATCH --nodes=1                          # Run all cores on a single node
+#SBATCH --ntasks=1                         # Run a single task        
+#SBATCH --cpus-per-task=16                 # Number of CPU cores per task
+#SBATCH --mem=120G                          # Job memory request
+#SBATCH --time=36:00:00                    # Time limit hrs:min:sec
+#SBATCH --mail-type=BEGIN,END,FAIL         # Send email on begin, end, and fail
+#SBATCH --mail-user=a.jaeger@campus.tu-berlin.de       # Your email address
+
+# load your java version
+module load java/21
+
+# execute a command
+java -Xmx120G -cp ./matsim-berlin-6.3-58ca69b-dirty.jar org.matsim.run.OpenBerlinScenario run --config=input/berlin-v6.1.config_felix_cluster.xml
